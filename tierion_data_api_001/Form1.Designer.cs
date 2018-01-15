8307fb134616cf252949763e5ef4deccff32c1f0
Hirono <32709712+watcher-hirono@users.noreply.github.com>
2018-01-15 22:30:27 +0900

    Add files via upload
    first commit

---
+++ tierion_data_api_001/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace tierion_data_api_001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxApiKey = new System.Windows.Forms.TextBox();
+            this.txtBoxValue = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtBoxResponse = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSubmitId = new System.Windows.Forms.Button();
+            this.txtBoxId = new System.Windows.Forms.TextBox();
+            this.txtBoxResponseId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBoxEmail = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtBoxDataStoreId = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtBoxKey = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtBoxApiKey
+            // 
+            this.txtBoxApiKey.Location = new System.Drawing.Point(94, 40);
+            this.txtBoxApiKey.Name = "txtBoxApiKey";
+            this.txtBoxApiKey.Size = new System.Drawing.Size(583, 19);
+            this.txtBoxApiKey.TabIndex = 3;
+            // 
+            // txtBoxValue
+            // 
+            this.txtBoxValue.Location = new System.Drawing.Point(97, 137);
+            this.txtBoxValue.Name = "txtBoxValue";
+            this.txtBoxValue.Size = new System.Drawing.Size(220, 19);
+            this.txtBoxValue.TabIndex = 10;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(242, 162);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 11;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "API Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Data";
+            // 
+            // txtBoxResponse
+            // 
+            this.txtBoxResponse.Location = new System.Drawing.Point(24, 191);
+            this.txtBoxResponse.Multiline = true;
+            this.txtBoxResponse.Name = "txtBoxResponse";
+            this.txtBoxResponse.Size = new System.Drawing.Size(293, 111);
+            this.txtBoxResponse.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(427, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 12);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Id";
+            // 
+            // btnSubmitId
+            // 
+            this.btnSubmitId.Location = new System.Drawing.Point(602, 162);
+            this.btnSubmitId.Name = "btnSubmitId";
+            this.btnSubmitId.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmitId.TabIndex = 15;
+            this.btnSubmitId.Text = "Submit";
+            this.btnSubmitId.UseVisualStyleBackColor = true;
+            this.btnSubmitId.Click += new System.EventHandler(this.btnSubmitReceipt_Click);
+            // 
+            // txtBoxId
+            // 
+            this.txtBoxId.Location = new System.Drawing.Point(447, 137);
+            this.txtBoxId.Name = "txtBoxId";
+            this.txtBoxId.Size = new System.Drawing.Size(230, 19);
+            this.txtBoxId.TabIndex = 14;
+            // 
+            // txtBoxResponseId
+            // 
+            this.txtBoxResponseId.Location = new System.Drawing.Point(378, 191);
+            this.txtBoxResponseId.Multiline = true;
+            this.txtBoxResponseId.Name = "txtBoxResponseId";
+            this.txtBoxResponseId.Size = new System.Drawing.Size(299, 111);
+            this.txtBoxResponseId.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(39, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "E-Mail";
+            // 
+            // txtBoxEmail
+            // 
+            this.txtBoxEmail.Location = new System.Drawing.Point(94, 12);
+            this.txtBoxEmail.Name = "txtBoxEmail";
+            this.txtBoxEmail.Size = new System.Drawing.Size(583, 19);
+            this.txtBoxEmail.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 12);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Data Store ID";
+            // 
+            // txtBoxDataStoreId
+            // 
+            this.txtBoxDataStoreId.Location = new System.Drawing.Point(94, 69);
+            this.txtBoxDataStoreId.Name = "txtBoxDataStoreId";
+            this.txtBoxDataStoreId.Size = new System.Drawing.Size(220, 19);
+            this.txtBoxDataStoreId.TabIndex = 5;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(60, 118);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(24, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Key";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(50, 140);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 12);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Value";
+            // 
+            // txtBoxKey
+            // 
+            this.txtBoxKey.Location = new System.Drawing.Point(97, 115);
+            this.txtBoxKey.Name = "txtBoxKey";
+            this.txtBoxKey.Size = new System.Drawing.Size(220, 19);
+            this.txtBoxKey.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 327);
+            this.Controls.Add(this.txtBoxKey);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtBoxDataStoreId);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtBoxResponseId);
+            this.Controls.Add(this.txtBoxId);
+            this.Controls.Add(this.btnSubmitId);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBoxResponse);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.txtBoxValue);
+            this.Controls.Add(this.txtBoxEmail);
+            this.Controls.Add(this.txtBoxApiKey);
+            this.Name = "Form1";
+            this.Text = "Tierion DATA API Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBoxApiKey;
+        private System.Windows.Forms.TextBox txtBoxValue;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtBoxResponse;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSubmitId;
+        private System.Windows.Forms.TextBox txtBoxId;
+        private System.Windows.Forms.TextBox txtBoxResponseId;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBoxEmail;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtBoxDataStoreId;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtBoxKey;
+    }
+}
+
---
+++ tierion_data_api_001/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tierion_data_api_001
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string url = "https://api.tierion.com/v1/records";
+
+            System.Net.WebClient wc = new System.Net.WebClient();
+
+            wc.Headers.Add("X-Username: " + this.txtBoxEmail.Text);
+            wc.Headers.Add("X-Api-Key: " + this.txtBoxApiKey.Text);
+
+            System.Collections.Specialized.NameValueCollection ps = new System.Collections.Specialized.NameValueCollection();
+            ps.Add("datastoreId", this.txtBoxDataStoreId.Text);
+            ps.Add(this.txtBoxKey.Text, this.txtBoxValue.Text);
+
+            byte[] resData = wc.UploadValues(url, ps);
+            wc.Dispose();
+
+            string resText = System.Text.Encoding.UTF8.GetString(resData);
+            this.txtBoxResponse.Text = resText;
+        }
+
+        private void btnSubmitReceipt_Click(object sender, EventArgs e)
+        {
+            string url = "https://api.tierion.com/v1/records/" + this.txtBoxId.Text;
+
+            System.Net.WebClient wc = new System.Net.WebClient();
+
+            wc.Headers.Add("X-Username: " + this.txtBoxEmail.Text);
+            wc.Headers.Add("X-Api-Key: " + this.txtBoxApiKey.Text);
+
+            byte[] resData = wc.DownloadData(url);
+            wc.Dispose();
+
+            string resText = System.Text.Encoding.UTF8.GetString(resData);
+            this.txtBoxResponseId.Text = resText;
+        }
+    }
+}
